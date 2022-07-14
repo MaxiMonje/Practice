a7cd85dbdb05d0cb89737ab6664933d37bb90726
Maximiliano Monje <maxiiigabrielmonje@gmail.com>
2022-07-14 15:24:10 -0300

    first commit

---
+++ src/main/kotlin/com/example/ProjectPractice/ProjectPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ProjectPractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ProjectPractice/ProjectPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ProjectPractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectPracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
